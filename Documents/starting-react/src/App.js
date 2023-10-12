99fc86c5eb547cdd76defdad4a8d448ba599c34a
Tono Abad <110565791+tono2002@users.noreply.github.com>
2023-10-12 04:01:31 +0300

    Created a initial interface

--- Documents/starting-react/src/App.js
+++ Documents/starting-react/src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className='title'>My first React Website</h1>
+      <h2 className='author'>By Tono Abad</h2>
     </div>
   );
 }
