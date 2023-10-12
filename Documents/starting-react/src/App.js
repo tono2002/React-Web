ef0145a4b7b44482562318fc31d91617cc39bbbd
Tono Abad <110565791+tono2002@users.noreply.github.com>
2023-10-12 04:03:35 +0300

    prueba

--- Documents/starting-react/src/App.js
+++ Documents/starting-react/src/App.js
@@ -2,9 +2,34 @@ import './App.css';
 
 function App() {
   return (
-    <div>
+    <div
+      style={{
+        margin: "auto",
+        width: 800,
+        padding: "1rem"
+      }}
+    >
       <h1 className='title'>My first React Website</h1>
       <h2 className='author'>By Tono Abad</h2>
+
+      <table width="100%">
+
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Age</th>
+          </tr>
+        </thead>
+
+        <tbody>
+        <tr>
+            <td>Tono Abad</td>
+            <td>21</td>
+          </tr>
+        </tbody>
+
+      </table>
+
     </div>
   );
 }
